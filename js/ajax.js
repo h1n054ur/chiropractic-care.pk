c2e4011150642ee06e665ecdd471ef3a4dceef46
h1n054ur <admin@haniumer.com>
2023-10-18 04:54:30 +0500

    update form

--- js/ajax.js
+++ js/ajax.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    // Convert FormData to a plain JavaScript object
     function formDataToObject(formData) {
         let obj = {};
         for (let [key, value] of formData.entries()) {
-            // If the key already exists, convert the value to an array.
             if (obj[key]) {
                 if (!Array.isArray(obj[key])) {
                     obj[key] = [obj[key]];
@@ -17,17 +15,14 @@ document.addEventListener('DOMContentLoaded', function() {
         return obj;
     }
 
-    // Grabbing the form element
     const form = document.querySelector('.pain-form');
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Preventing the default form submission
+        event.preventDefault();
 
-        // Grabbing all the form data and converting to an object
         const formData = new FormData(form);
         const formDataObject = formDataToObject(formData);
 
-        // Sending the data via Fetch API
         fetch('https://prod-08.australiasoutheast.logic.azure.com:443/workflows/52dcee3b0f4748f99c52d15b6bd6e38b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ER-oVVkM_47WIelinwDFbzllOWgz-ZPEO3yJhjJVW9U', {
             method: 'POST',
             headers: {
@@ -35,11 +30,18 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             body: JSON.stringify(formDataObject)
         })
-        .then(response => response.json()) // Assuming server responds with json
+        .then(response => {
+            if (response.ok) {
+                return response.json().catch(error => {
+                    return null; // If error occurs during parsing JSON, just return null and continue
+                });
+            } else {
+                throw new Error('Server returned a non-200 status');
+            }
+        })
         .then(data => {
-            console.log(data);
             alert('Form successfully submitted!');
-            form.reset(); // Clearing the form
+            form.reset(); // Clear the form after successful submission
         })
         .catch((error) => {
             console.error('Error:', error);
